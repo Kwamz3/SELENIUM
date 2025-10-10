a64ed898cebea55e823358f42d12ede15037f488
Kwamz3 <charlie18appiah@gmail.com>
2025-10-10 14:10:04 +0000

    Refactor WebDriver initialization in start_1.py to use Service for better management

--- selenium_start/start_1.py
+++ selenium_start/start_1.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import time
 
-driver = webdriver.Chrome()
+PATH = r"C:\Subdrive\chromedriver-win64\chromedriver.exe"
+
+service = Service(PATH)
+driver = webdriver.Chrome(service=service)
 
 driver.get("http://selenium.dev")
 
