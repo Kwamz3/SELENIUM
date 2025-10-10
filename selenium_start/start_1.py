7464e85418cf27428f987648346224a30db54644
Kwamz3 <charlie18appiah@gmail.com>
2025-10-10 15:01:11 +0000

    Remove commented-out WebDriver service initialization in start_1.py

--- selenium_start/start_1.py
+++ selenium_start/start_1.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 import time
 
-PATH = r"C:\Subdrive\chromedriver-win64\chromedriver.exe"
+# PATH = r"C:\Subdrive\chromedriver-win64\chromedriver.exe"
 
-service = Service(PATH)
-driver = webdriver.Chrome(service=service)
+# service = Service(PATH)
+driver = webdriver.Chrome()#service=service)
 
 driver.get("http://selenium.dev")
-
 
 time.sleep(50)
