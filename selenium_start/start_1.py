606405e1ef585b55eae33574818123c663601ded
Kwamz3 <charlie18appiah@gmail.com>
2025-10-11 01:57:37 +0000

    Refactor WebDriver usage in start_1.py to improve element handling and maximize window

--- selenium_start/start_1.py
+++ selenium_start/start_1.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 
 driver.get("https://www.wakanow.com.gh/en-gh?gad_campaignid=22361038964")
 
-time.sleep(50)
+wait = WebDriverWait(driver, 10)
+pop_close = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "btn-close")))
+pop_close.click()
+
+time.sleep(70)
 driver.quit()
