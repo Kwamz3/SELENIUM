166fc3bfa96f51cdb6b98e157df2966a9e94a9a8
Kwamz3 <charlie18appiah@gmail.com>
2025-10-13 12:40:41 +0000

    Refactor check function for title validation and adjust implicit wait time

--- selenium_start/start_1.py
+++ selenium_start/start_1.py
@@ -16,19 +16,23 @@ def setup():
 driver = setup()
 driver.get("https://www.wakanow.com.gh/en-gh?gad_campaignid=22361038964")
 # driver.get("https://www.google.com")
-driver.implicitly_wait(10)
+driver.implicitly_wait(8)
 
-title = driver.title
-page_title = "Book Cheap Flights, Hotels and Vacation Packages | Wakanow"
-# assert title == page_title
-if (title == page_title) == True:
-    print("Title matches!")
-else:
-    print(f"Expected title'{page_title}' but opened '{title}'")
+def check():
+    title = driver.title
+    page_title = "Book Cheap Flights, Hotels and Vacation Packages | Wakanow"
+    # assert title == page_title
+    if (title == page_title) == True:
+        print("Title matches!")
+    else:
+        print(f"Expected title'{page_title}' but opened '{title}'")
 
 wait = WebDriverWait(driver, 10)
 pop_close = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "btn-close")))
 pop_close.click()
+wait_2 = WebDriverWait(driver, 10)
+pop_close_2 = wait_2.until(EC.presence_of_element_located((By.CLASS_NAME, "cm__btn")))
+pop_close_2.click()
 
 # wait2 = WebDriverWait(driver, 10)
 # pop_close2 = wait2.until(EC.presence_of_element_located((By.ID, "webklipper-publisher-widget-container-notification-close-div")))
