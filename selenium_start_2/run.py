1a728043738018c6c7e770e59ddfd76443e6bcd6
Kwamz3 <charlie18appiah@gmail.com>
2025-10-24 09:02:59 +0000

    Add comments for working dates in run.py

--- selenium_start_2/run.py
+++ selenium_start_2/run.py
@@ -11,3 +11,8 @@ with Booking() as bot:
     bot.select_ret_date(day="Saturday", month="November", date="8", year=2025) 
     bot.select_passengers(adl_cnt=3, chd_cnt=1, inf_cnt=1)
     bot.intentional_wait(30)
+    
+    
+    # Working dates
+    # Saturday, November 8, 2025
+    # Friday, December 19, 2025
