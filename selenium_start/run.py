9c8b76e3354578b34fe887f0e3417e95c1339e68
Kwamz3 <charlie18appiah@gmail.com>
2025-10-24 11:05:41 +0000

    Update return date in run.py to January 3, 2026 for improved accuracy

--- selenium_start/run.py
+++ selenium_start/run.py
@@ -8,7 +8,7 @@ with Booking() as bot:
     bot.select_city(place_from="Accra")
     bot.select_destination(place_to="Dubai")
     bot.select_dep_date(day="Friday", month="October", date="31", year=2025)
-    bot.select_ret_date(day="Saturday", month="November", date="8", year=2025) 
+    bot.select_ret_date(day="Saturday", month="January", date="3", year=2026) 
     bot.select_passengers(adl_cnt=3, chd_cnt=1, inf_cnt=1)
     bot.intentional_wait(30)
     
